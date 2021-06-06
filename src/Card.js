1ba211020baefcac333324a5f003ab2a2b05a2ab
armadi1809 <armadi@purdue.edu>
2021-06-06 19:27:38 -0400

    Uploading project files

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shaow-5'>
+      <img alt='Robot' src={`https://robohash.org/${props.id}?200x200`} />
+      <div>
+        <h2> {props.name} </h2>
+        <p> {props.email}</p>
+      </div>
+    </div>
+  );
+}
+export default Card;
